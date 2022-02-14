f92354abad53dcde4d70ddc7ad40c4eab04076c1
Elias Gregorio Pena Evertz <epena@glscrip.com>
2022-02-13 21:51:22 -0500

    added JS class Assignment 3 Files

---
+++ login/logn.js
@@ -0,0 +1,27 @@
+function login() {
+    var propertyWidth = 960;
+    var propertyHeight = 600;
+    var winLeft = ((screen.width - propertyWidth) / 2);
+    var winTop = ((screen.height - propertyHeight) / 2);
+    var winOptions = "width=960,height=600";
+    winOptions += ",left=" + winLeft;
+    winOptions += ",top=" + winTop;
+    var loginWindow = window.open("login/login.html", "login", winOptions);
+    loginWindow.focus();
+}
+
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
+
+function createEventListeners() {
+    var loginB = document.getElementById("login");
+
+    if (loginB.addEventListener) {
+        loginB.addEventListener("click", login, false);
+    } else if (mainFig.attachEvent) {
+        loginB.attachEvent("onclick", login);
+    }
+}
